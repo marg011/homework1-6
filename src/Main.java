4eb4894dd2b738d5f2d80debeb915f6b6458a21e
margo <margo171190@mail.ru>
2023-04-27 21:35:36 +0400

    Homework 1-6 is done!

--- src/Main.java
+++ src/Main.java
@@ -14,47 +14,87 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
+        for (int i=1; i < 11; i++){
+            System.out.println(i);
+        }
 
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
+        for (int i = 10; i > 0; i--){
+            System.out.println(i);
+        }
 
     }
     public static void task3 () {
         System.out.println("Задача 3");
-
+        for (int i = 0; i <= 17; i = i+2){
+            System.out.println(i);
+        }
     }
 
     public static void task4 () {
         System.out.println("Задача 4");
+        for (int i = 10; i > -11; i--){
+            System.out.println(i);
+        }
 
     }
     public static void task5 () {
         System.out.println("Задача 5");
+        for (int i = 1904; i < 2097; i = i + 4) {
+            if (i % 100 != 0) {
+                System.out.println(i + " is a leap year");
+            }
 
+        }
     }
 
     public static void task6 () {
         System.out.println("Задача 6");
+        for (int i = 7; i < 99; i=i+7) {
+            System.out.println(i);
+        }
 
     }
     public static void task7 () {
         System.out.println("Задача 7");
+        for (int i = 1; i < 513; i = i*2){
+            System.out.println(i);
+        }
 
     }
 
     public static void task8 () {
         System.out.println("Задача 8");
+        int contribution = 29000;
+        int currentAccumulation = 0;
+        for (int i = 1; i < 13; i++){
+            currentAccumulation = currentAccumulation + contribution;
+            System.out.println("Month " + i + "the sum of accumulation is " + currentAccumulation + " rubles.");
+
+        }
 
     }
     public static void task9 () {
         System.out.println("Задача 9");
+        int contribution = 29000;
+        int currentAccumulation = 0;
+        for (int i = 1; i < 13; i++){
+            currentAccumulation = currentAccumulation + currentAccumulation/100;
+            currentAccumulation = currentAccumulation + contribution;
+            System.out.println("Month " + i + "the sum of accumulation is " + currentAccumulation + " rubles.");
+
+        }
 
     }
 
     public static void task10 () {
         System.out.println("Задача 10");
+        for (int i = 1; i < 11; i++){
+            System.out.println("2*" + i + "=" + 2*i);
+        }
 
     }
 }
